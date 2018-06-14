ef0113338adb14c2f9ae78759c484856d2bd269f
DESKTOP-HFR41QL\sugab <websnuzy@gmail.com>
2018-06-14 08:33:25 +0100

    initial project

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'b04c4e338a7f43ac8dfc9b64a2ea0d46'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+	  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	  .then(data => {
+	  	res.json(data);
+	  })
+	  .catch(err => res.status(400).json('unable to work with api'))
+}
+
+const handleImagePut = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get count!'))
+}
+
+module.exports = {
+	handleImagePut,
+	handleApiCall
+};
